10a22bf1dedc849911e54f257ad0fc8a9c9afa61
petrk94 <petrk94@users.noreply.github.com>
2016-10-19 23:42:20 +0200

    added program code for LDR value to Lux value conversion

--- Solarrouter/Solarrouter.ino
+++ Solarrouter/Solarrouter.ino
@@ -48,6 +48,14 @@ const long oneSecond = 1000;
 const long oneMinute = oneSecond*60;
 const long fiveMinutes = oneMinute*5;
 
+// These constants, define values needed for the LDR readings and ADC
+#define LDR_PIN                   0
+#define MAX_ADC_READING           1023
+#define ADC_REF_VOLTAGE           5.0
+#define REF_RESISTANCE            5030  // measure this for best results
+#define LUX_CALC_SCALAR           12518931
+#define LUX_CALC_EXPONENT         -1.405
+
 
 
 DHT dht(DHTPIN, DHTTYPE);
@@ -74,6 +82,12 @@ void setup(){
     
 void loop()
 {
+  int   ldrRawData;
+  float resistorVoltage, ldrVoltage;
+  float ldrResistance;
+  float ldrLux;
+
+  
   // Misst den Helligkeitswert vom LDR
   int sensorValueLDR = analogRead(analogInPinLDR);
 
@@ -98,6 +112,32 @@ void loop()
   voutsolar = (sensorValueUsolar * 5.0)/1024.0;
   vinsolar = voutsolar/(R2/(R1+R2));
   
+
+
+  // Berechnung Lux Werte von dem LDR Spannungsteiler 5V -- LDR --- Ax --- 10kOhm --- GND
+  // Programmcode stammt von http://www.allaboutcircuits.com/projects/design-a-luxmeter-using-a-light-dependent-resistor/
+  // Perform the analog to digital conversion  
+  ldrRawData = analogRead(analogInPinLDR);
+  
+  // RESISTOR VOLTAGE_CONVERSION
+  // Convert the raw digital data back to the voltage that was measured on the analog pin
+  resistorVoltage = (float)ldrRawData / MAX_ADC_READING * ADC_REF_VOLTAGE;
+
+  // voltage across the LDR is the 5V supply minus the 5k resistor voltage
+  ldrVoltage = ADC_REF_VOLTAGE - resistorVoltage;
+  
+  // LDR_RESISTANCE_CONVERSION
+  // resistance that the LDR would have for that voltage  
+  ldrResistance = ldrVoltage/resistorVoltage * REF_RESISTANCE;
+  
+  // LDR_LUX
+  // Change the code below to the proper conversion from ldrResistance to
+  // ldrLux
+  ldrLux = LUX_CALC_SCALAR * pow(ldrResistance, LUX_CALC_EXPONENT);
+
+
+
+
 
   // Serielle Datenausgabe
   // Ausgabe LDR
